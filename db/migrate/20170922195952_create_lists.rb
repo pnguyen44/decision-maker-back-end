4b54567abcbc026c79457ebad8bd942628f15b41
pnguyen44 <nguyen.phuongnhat@gmail.com>
2017-09-24 00:01:20 -0400

    Update migration for lists table Add name attribute to be unique: true

--- db/migrate/20170922195952_create_lists.rb
+++ db/migrate/20170922195952_create_lists.rb
@@ -1,7 +1,7 @@
 class CreateLists < ActiveRecord::Migration[5.0]
   def change
     create_table :lists do |t|
-      t.string :name, null: false
+      t.string :name, null: false, index: { unique: true }
       t.references :user, index: true, foreign_key: true, null: false
       t.timestamps
     end
