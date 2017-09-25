e60c1f4d9c1149411db0fb87df3ecc0b5d9cb805
pnguyen44 <nguyen.phuongnhat@gmail.com>
2017-09-24 22:27:26 -0400

    Update list migration remove unique constraint on name attribute

--- db/migrate/20170922195952_create_lists.rb
+++ db/migrate/20170922195952_create_lists.rb
@@ -1,7 +1,7 @@
 class CreateLists < ActiveRecord::Migration[5.0]
   def change
     create_table :lists do |t|
-      t.string :name, null: false, index: { unique: true }
+      t.string :name, null: false
       t.references :user, index: true, foreign_key: true, null: false
       t.timestamps
     end
