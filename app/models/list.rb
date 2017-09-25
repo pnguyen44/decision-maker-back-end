433c0d5e1b27b45c2d6e7d74662b531526349c66
pnguyen44 <nguyen.phuongnhat@gmail.com>
2017-09-24 22:20:25 -0400

    Update list model add relationship

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,21 @@
+# class List < ApplicationRecord
+# frozen_string_literal: true
+#   belongs_to :user, foreign_key: 'user_id',inverse_of: :lists
+#   has_many :items, foreign_key:'item_id', inverse_of: :lists
+#   validates :name, :user, presence: true
+#   validates_uniqueness_of :name
+# end
+
+# class List < ApplicationRecord
+# frozen_string_literal: true
+#  belongs_to :user, inverse_of: :lists
+#  has_many :items,  foreign_key: 'list_id'
+#  validates :name, :user, presence: true
+#  validates_uniqueness_of :name
+# end
+
 class List < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, inverse_of: :lists
+  has_many :items, foreign_key: 'list_id'
   validates :name, :user, presence: true
 end
