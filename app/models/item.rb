e2ea97c3de9a72540cf6e073d3c488d4398b1604
pnguyen44 <nguyen.phuongnhat@gmail.com>
2017-09-25 14:22:44 -0400

    Update item model update relationship on model

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,9 +5,15 @@
 #   validates :name, presence: true
 # end
 
+# class Item < ApplicationRecord
+#   belongs_to :list, inverse_of: :items
+#   #belongs_to :user, inverse_of: :items
+#   has_one :user, through: :list
+#   validates :name, presence: true
+# end
+
 class Item < ApplicationRecord
-  belongs_to :list, inverse_of: :items
-  #belongs_to :user, inverse_of: :items
+  belongs_to :list
   has_one :user, through: :list
   validates :name, presence: true
 end
