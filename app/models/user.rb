d1bb2f856ebc70de0d5aded4f9006a0502b1e164
pnguyen44 <nguyen.phuongnhat@gmail.com>
2017-09-24 22:22:09 -0400

    Update user model add relationships to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
+# class User < ApplicationRecord
+#   include Authentication
+#   has_many :examples
+#   has_many :lists, inverse_of: :user
+#   has_many :items, through: :lists
+# end
+
+# frozen_string_literal: true
 class User < ApplicationRecord
   include Authentication
   has_many :examples
   has_many :lists
+  has_many :items, through: :list
 end
